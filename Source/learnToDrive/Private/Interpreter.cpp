65504f2ac7920a21518e69196e49d471dd945fcd
CristianNicolae <76454832+crissty21@users.noreply.github.com>
2022-11-21 18:35:47 +0200

    got edge detection working

--- Source/learnToDrive/Private/Interpreter.cpp
+++ Source/learnToDrive/Private/Interpreter.cpp
@@ -3,7 +3,7 @@
 AInterpreter::AInterpreter()
 {
 	PrimaryActorTick.bCanEverTick = true;
-	VideoSize = FVector2D(256, 256);
+	VideoSize = FVector2D(512, 128);
 	RefreshRate = 30.0f;
 }
 
@@ -25,7 +25,10 @@ void AInterpreter::BeginPlay()
 
 	//cvSize = cv::Size(VideoSize.X, VideoSize.Y);
 	//cvMat = new cv::Mat(cvSize, CV_8UC4, ColorData.GetData());
-
+	CreateLUT(LUTb, BChannelThresh);
+	CreateLUT(LUTs, SChannelThresh);
+	CreateLUT(LUTl, LChannelThresh);
+	CreateLUT(LUTa, AChannelThresh);
 }
 
 // Called every frame
@@ -44,15 +47,13 @@ void AInterpreter::Tick(float DeltaTime)
 		{
 			if (RefreshTimer >= 1.0f / RefreshRate) 
 			{
-				UE_LOG(LogTemp, Warning, TEXT("%f %f"), RefreshTimer, 1.0f / RefreshRate);
-				TextureRenderRef->UpdateTexture2D(Texture, TextureRenderRef->GetTextureFormatForConversionToTexture2D(), EConstructTextureFlags::CTF_DeferCompression);
+				//TextureRenderRef->UpdateTexture2D(Texture, TextureRenderRef->GetTextureFormatForConversionToTexture2D(), EConstructTextureFlags::CTF_DeferCompression);
+				ReadFrame();
 				if (Texture->PlatformData->Mips[0].BulkData.IsLocked() == false)
 				{
 					Texture->UpdateResource();
 				}
 				RefreshTimer -= 1.0f / RefreshRate;
-				//ReadFrame();
-				//i might actually be a fucking genius 
 			}
 		}
 		else
@@ -64,89 +65,243 @@ void AInterpreter::Tick(float DeltaTime)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("renderTarget invalid"));
 	}
+
 }
 
 void AInterpreter::ReadFrame()
 {
 	//FColor* FormatedImageData = static_cast<FColor*>(Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE));
-	//Texture->PlatformData->Mips[0].BulkData.Unlock();
 
-	//cv::Mat* colorData = new cv::Mat(cvSize, CV_8UC4, ColorData.GetData());
 
-	//colorData->convertTo(*colorData, -1, 1, 0);
+	FRenderTarget* renderTarget = TextureRenderRef->GameThread_GetRenderTargetResource();
+	renderTarget->ReadPixels(ColorData);
 
-	//const int32 TextureDataSize = colorData->size().width * colorData->size().height * 4 * sizeof(uint8);
+	//Mat colorData = Mat(Size(512, 128), CV_8UC4, FormatedImageData);
 
-	// Lock the texture so we can read / write to it
-	// set the texture data
 
-	//FTexture2DMipMap* MyMipMap = &Texture->PlatformData->Mips[0];
-	//FByteBulkData* RawImageData = &MyMipMap->BulkData;
-	/*
-	uint8* Pixels = new uint8[VideoSize.X * VideoSize.Y * 4];
-	for (int32 y = 0; y < VideoSize.X; y++)
+	Mat colorData = Mat(Size(512,128), CV_8UC4, ColorData.GetData());
+
+	Mat sBinary = BinaryThresholdLAB_LUV(colorData, BChannelThresh, LChannelThresh);
+	Mat h, l, s;
+	GetHLS(colorData, h, l, s);
+	Mat sxBinary = GradientThreshold(s, 2, GradientThresh);
+	
+	Mat finalImage = Mat(Size(512, 128), CV_8UC4);
+	
+	for (int i = 0; i < finalImage.cols; i++)
 	{
-		for (int32 x = 0; x < VideoSize.Y; x++)
+		for (int j = 0; j < finalImage.rows; j++)
 		{
-			int32 curPixelIndex = ((y * VideoSize.Y) + x);
-			Pixels[4 * curPixelIndex] = ColorData[4 * curPixelIndex].B;
-			Pixels[4 * curPixelIndex + 1] = ColorData[4 * curPixelIndex].G;
-			Pixels[4 * curPixelIndex + 2] = ColorData[4 * curPixelIndex].R;
-			Pixels[4 * curPixelIndex + 3] = ColorData[4 * curPixelIndex].A;
+			finalImage.at<Vec<uint8, 4>>(j, i)[0] = ((uint8)(
+				sBinary.at<uint8>(j, i)
+				|| 
+				sxBinary.at<uint8>(j, i)
+				) * 255);
+			finalImage.at<Vec<uint8, 4>>(j, i)[1] = ((uint8)(
+				sBinary.at<uint8>(j, i)
+				||
+				sxBinary.at<uint8>(j, i)
+				) * 255);
+			finalImage.at<Vec<uint8, 4>>(j, i)[2] = ((uint8)(
+				sBinary.at<uint8>(j, i)
+				||
+				sxBinary.at<uint8>(j, i)
+				) * 255);
+			finalImage.at<Vec<uint8, 4>>(j, i)[3] = 255;
 		}
 	}
-	// Allocate first mipmap.
-	FTexture2DMipMap* Mip = new(Texture->PlatformData->Mips) FTexture2DMipMap();
-	Mip->SizeX = VideoSize.X;
-	Mip->SizeY = VideoSize.Y;
+	void* textureData = Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE);
+	const int32 dataSize = 512 * 128 *4* sizeof(uint8);
+	FMemory::Memcpy(textureData, finalImage.data, dataSize);
 
-	Mip->BulkData.Lock(LOCK_READ_WRITE);
-	uint8* TextureData = (uint8*)Mip->BulkData.Realloc(VideoSize.X * VideoSize.Y * 4);
-	FMemory::Memcpy(TextureData, Pixels, sizeof(uint8) * VideoSize.X * VideoSize.Y * 4);
-	Mip->BulkData.Unlock();
-
-	*/
-	/*
-	//FColor* FormatedImageData = static_cast<FColor*>(RawImageData->Lock(LOCK_READ_WRITE));
-
-	FColor* FormatedImageData = static_cast<FColor*>(Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE));
-	uint32 TextureWidth = MyMipMap->SizeX, TextureHeight = MyMipMap->SizeY;
-	for (uint32 x = 0; x < TextureWidth; x++)
+	Texture->PlatformData->Mips[0].BulkData.Unlock();
+	Texture->UpdateResource();
 	{
-		for (uint32 y = 0; y < TextureHeight; y++)
+		/*
+		Vec3b data = outH.at<Vec3b>(0, 0);
+		UE_LOG(LogTemp, Warning, TEXT("%i"), data[0]);
+		*/
+		//colorData->convertTo(*colorData, -1, 1, 0);
+
+		//const int32 TextureDataSize = colorData->size().width * colorData->size().height * 4 * sizeof(uint8);
+
+		// Lock the texture so we can read / write to it
+		// set the texture data
+
+		//FTexture2DMipMap* MyMipMap = &Texture->PlatformData->Mips[0];
+		//FByteBulkData* RawImageData = &MyMipMap->BulkData;
+		/*
+		uint8* Pixels = new uint8[VideoSize.X * VideoSize.Y * 4];
+		for (int32 y = 0; y < VideoSize.X; y++)
 		{
-			FormatedImageData[y * TextureWidth + x] = ColorData[y * TextureWidth + x];
+			for (int32 x = 0; x < VideoSize.Y; x++)
+			{
+				int32 curPixelIndex = ((y * VideoSize.Y) + x);
+				Pixels[4 * curPixelIndex] = ColorData[4 * curPixelIndex].B;
+				Pixels[4 * curPixelIndex + 1] = ColorData[4 * curPixelIndex].G;
+				Pixels[4 * curPixelIndex + 2] = ColorData[4 * curPixelIndex].R;
+				Pixels[4 * curPixelIndex + 3] = ColorData[4 * curPixelIndex].A;
+			}
+		}
+		// Allocate first mipmap.
+		FTexture2DMipMap* Mip = new(Texture->PlatformData->Mips) FTexture2DMipMap();
+		Mip->SizeX = VideoSize.X;
+		Mip->SizeY = VideoSize.Y;
+
+		Mip->BulkData.Lock(LOCK_READ_WRITE);
+		uint8* TextureData = (uint8*)Mip->BulkData.Realloc(VideoSize.X * VideoSize.Y * 4);
+		FMemory::Memcpy(TextureData, finalImage.data, sizeof(uint8) * VideoSize.X * VideoSize.Y * 4);
+		Mip->BulkData.Unlock();
+
+		
+		/*
+		//FColor* FormatedImageData = static_cast<FColor*>(RawImageData->Lock(LOCK_READ_WRITE));
+
+		FColor* FormatedImageData = static_cast<FColor*>(Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_WRITE));
+		uint32 TextureWidth = MyMipMap->SizeX, TextureHeight = MyMipMap->SizeY;
+		for (uint32 x = 0; x < TextureWidth; x++)
+		{
+			for (uint32 y = 0; y < TextureHeight; y++)
+			{
+				FormatedImageData[y * TextureWidth + x] = ColorData[y * TextureWidth + x];
+			}
+		}
+		RawImageData->Unlock();
+
+		FormatedImageData = static_cast<FColor*>(Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_ONLY));
+		UE_LOG(LogTemp, Warning, TEXT("%sDA"), *(FormatedImageData[0].ToString()));
+		RawImageData->Unlock();
+
+		//Texture->UpdateResource();
+		//FMemory::Memcpy(TextureData, cvMat.data, TextureDataSize);
+		//FMemory::ParallelMemcpy(TextureData, colorData->data, TextureDataSize);
+
+		// Unlock the texture
+		//PlayerControllerRef->Texture->PlatformData->Mips[0].BulkData.Unlock();
+		// Apply Texture changes to GPU memory
+		/*
+		PlayerControllerRef->Texture->UpdateResource();
+		Camera_Texture2D = PlayerControllerRef->Texture;
+		PlayerControllerRef->UpdateTexture();
+		//PlayerControllerRef->Texture = Camera_Texture2D;
+		//pass this to MyHUD
+		/*
+		AMyHUD* hud = Cast<AMyHUD>(UGameplayStatics::GetPlayerController(this, 0)->GetHUD());
+		if (hud)
+		{
+			hud->DrawTexture(texture, 10, 10, 256, 256, 1, 1, 256, 256);
+		}*/
+
+		//how to change texture to a image on a hud
+		//update brush via texture on a image in a widget class
+
+	}
+}
+
+void AInterpreter::GetHLS(Mat inputRGB, Mat& outputH, Mat& outputL, Mat& outputS)
+{
+	Mat Output; 
+	cvtColor(inputRGB, Output, cv::COLOR_BGR2HLS);
+	Mat channel[3];
+	split(Output, channel);
+	outputH = channel[0];
+	outputL = channel[1];
+	outputS = channel[2];
+}
+
+void AInterpreter::GetLAB(Mat inputRGB, Mat& outputL, Mat& outputA, Mat& outputB)
+{
+	Mat Output;
+	cvtColor(inputRGB, Output, cv::COLOR_RGB2Lab);
+	Mat channel[3];
+	split(Output, channel);
+	outputL = channel[0];
+	outputA = channel[1];
+	outputB = channel[2];
+}
+
+void AInterpreter::GetLUV(Mat inputRGB, Mat& outputL, Mat& outputU, Mat& outputV)
+{
+	Mat Output;
+	cvtColor(inputRGB, Output, cv::COLOR_BGR2Luv);
+	Mat channel[3];
+	split(Output, channel);
+	outputL = channel[0];
+	outputU = channel[1];
+	outputV = channel[2];
+}
+
+cv::Mat AInterpreter::BinaryThresholdLAB_LUV(Mat inputRGB, const FVector2D bThreshold, const FVector2D lThreshold)
+{
+	//creates binary mask 
+	Mat binaryImage = Mat(inputRGB.size(),CV_8UC1);
+	Mat outL, outA, outB;
+	GetLAB(inputRGB, outL, outA, outB);
+	
+	for (int i = 0; i < inputRGB.cols; i++)
+	{
+		for (int j = 0; j < inputRGB.rows; j++)
+		{
+			binaryImage.at<uint8>(j, i) = ((uint8)(LUTb[outB.at<uint8>(j, i)] == 1 && LUTl[outL.at<uint8>(j, i)] == 1 && LUTa[outA.at<uint8>(j, i)] == 1));
 		}
 	}
-	RawImageData->Unlock();
+	return binaryImage;
+}
 
-	FormatedImageData = static_cast<FColor*>(Texture->PlatformData->Mips[0].BulkData.Lock(LOCK_READ_ONLY));
-	UE_LOG(LogTemp, Warning, TEXT("%sDA"), *(FormatedImageData[0].ToString()));
-	RawImageData->Unlock();
-
-	//Texture->UpdateResource();
-	//FMemory::Memcpy(TextureData, cvMat.data, TextureDataSize);
-	//FMemory::ParallelMemcpy(TextureData, colorData->data, TextureDataSize);
-
-	// Unlock the texture
-	//PlayerControllerRef->Texture->PlatformData->Mips[0].BulkData.Unlock();
-	// Apply Texture changes to GPU memory
-	/*
-	PlayerControllerRef->Texture->UpdateResource();
-	Camera_Texture2D = PlayerControllerRef->Texture;
-	PlayerControllerRef->UpdateTexture();
-	//PlayerControllerRef->Texture = Camera_Texture2D;
-	//pass this to MyHUD
-	/*
-	AMyHUD* hud = Cast<AMyHUD>(UGameplayStatics::GetPlayerController(this, 0)->GetHUD());
-	if (hud)
+cv::Mat AInterpreter::BinaryThresholdHLS(Mat inputRGB, const FVector2D sThreshold, const FVector2D lThreshold)
+{
+	Mat binaryImage = Mat(inputRGB.size(), CV_8UC1);
+	Mat outH, outL, outS;
+	GetHLS(inputRGB, outH, outL, outS);
+	for (int i = 0; i < inputRGB.cols; i++)
 	{
-		hud->DrawTexture(texture, 10, 10, 256, 256, 1, 1, 256, 256);
+		for (int j = 0; j < inputRGB.rows; j++)
+		{
+			binaryImage.at<uint8>(j, i) = ((uint8)(LUTs[outS.at<uint8>(j, i)] && LUTl[outL.at<uint8>(j, i)]));
+		}
+	}
+	return binaryImage;
+}
+
+cv::Mat AInterpreter::GradientThreshold(Mat input, int channel, const FVector2D threshold)
+{
+	Mat binaryImage = Mat(input.size(), CV_8UC1);
+	Mat sobel;
+	//blur
+	blur(input, input, Size(KSizeForBlur.X,KSizeForBlur.Y));
+
+	Sobel(input, sobel, CV_8U, 1, 0);
+	/*for (int i = 0; i < sobel.rows; i++)
+	{
+		for (int j = 0; j < sobel.cols; j++)
+		{
+			sobel.at<uint8>(i,j) = FMath::Abs(sobel.at<int8>(i, j));
+		}
 	}*/
+	
+	for (int i = 0; i < input.cols; i++)
+	{
+		for (int j = 0; j < input.rows; j++)
+		{
+			uint8& pixel = binaryImage.at<uint8>(j, i) = ((uint8)(sobel.at<int8>(j, i) > threshold[0] && sobel.at<uint8>(j, i) < threshold[1]));
+		}
+	}
+	//binaryImage = sobel;
+	return binaryImage;
+}
 
-	//how to change texture to a image on a hud
-	//update brush via texture on a image in a widget class
-
-
+void AInterpreter::CreateLUT(uint8* LUT, FVector2D Threshold)
+{
+	for (int i = 0; i < 256; i++)
+	{
+		if (i >= Threshold[0] && i <= Threshold[1])
+		{
+			LUT[i] = 1;
+		}
+		else
+		{
+			LUT[i] = 0;
+		}
+	}
 }
 
--- Source/learnToDrive/Private/Interpreter.h
+++ Source/learnToDrive/Private/Interpreter.h
@@ -7,7 +7,18 @@
 #include "Engine/TextureRenderTarget2D.h"
 #include "Engine/Texture.h"
 
+#include "PreOpenCVHeaders.h"
+#include "OpenCVHelper.h"
+
+#include <ThirdParty/OpenCV/include/opencv2/imgproc.hpp>
+#include <ThirdParty/OpenCV/include/opencv2/highgui/highgui.hpp>
+
+#include <ThirdParty/OpenCV/include/opencv2/core.hpp>
+#include "PostOpenCVHeaders.h"
+
 #include "Interpreter.generated.h"
+
+using namespace cv;
 
 UCLASS()
 class AInterpreter : public AActor
@@ -49,7 +60,53 @@ public:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = Data)
 		TArray<FColor> ColorData;
 
+
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D GradientThresh = FVector2D(60, 150);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D KSizeForBlur = FVector2D(6, 6);
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D SChannelThresh = FVector2D(80, 255);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D LChannelThresh = FVector2D(120, 240);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D  BChannelThresh = FVector2D(80, 255);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D  AChannelThresh = FVector2D(110, 255);
+
+	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
+		FVector2D L2ChannelThresh = FVector2D(255, 255);
+
+	uint8 LUTb[256];
+	uint8 LUTl[256];
+	uint8 LUTs[256];
+	uint8 LUTa[256];
+
+public:
+	//functions
+	cv::Mat da;
 	// reads the current video frame
 	UFUNCTION(BlueprintCallable, Category = Data)
 		void ReadFrame();
+
+	void GetHLS(Mat inputRGB, Mat& outputH, Mat& outputL, Mat& outputS);
+
+	void GetLAB(Mat inputRGB, Mat& outputL, Mat& outputA, Mat& outputB);
+
+	void GetLUV(Mat inputRGB, Mat& outputL, Mat& outputU, Mat& outputV);
+
+	Mat BinaryThresholdLAB_LUV(Mat inputRGB, const FVector2D bThreshold, const FVector2D lThreshold);
+
+	Mat BinaryThresholdHLS(Mat inputRGB, const FVector2D sThreshold, const FVector2D lThreshold);
+
+	Mat GradientThreshold(Mat inputRGB, int channel, const FVector2D threshold);
+	
+	void CreateLUT(uint8* LUT, FVector2D Threshold);
+
+
 };
