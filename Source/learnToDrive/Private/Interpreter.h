f9b8e656b413fc043a28860d16f169d697f136f0
CristianNicolae <76454832+crissty21@users.noreply.github.com>
2023-02-02 18:45:40 +0200

    first clean up

--- Source/learnToDrive/Private/Interpreter.h
+++ Source/learnToDrive/Private/Interpreter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -18,7 +16,6 @@
 
 #include "Interpreter.generated.h"
 
-using namespace cv;
 
 UCLASS()
 class AInterpreter : public AActor
@@ -54,8 +51,6 @@ public:
 	UPROPERTY(BlueprintReadOnly)
 		UTexture2D* Texture3 = nullptr;
 
-	bool flipflop;
-		//FRenderTarget* RenderTarget = nullptr;
 
 	// The rate at which the color data array and video texture is updated (in frames per second)
 	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = Camera, meta = (ClampMin = 0, UIMin = 0))
@@ -71,44 +66,12 @@ public:
 	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = Data)
 		TArray<FColor> ColorData;
 
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D GradientThresh = FVector2D(60, 150);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D KSizeForBlur = FVector2D(6, 6);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		bool SobelThreshold = true;
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		bool LabThreshold = true;
-
 	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
 		bool DrawEdgesInRed = true;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
 		bool ShowMaxLane = false;
 
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D SChannelThresh = FVector2D(80, 255);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D LChannelThresh = FVector2D(120, 240);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D  BChannelThresh = FVector2D(80, 255);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D  AChannelThresh = FVector2D(110, 255);
-
-	UPROPERTY(EditDefaultsOnly, Category = "Create Binary Image")
-		FVector2D L2ChannelThresh = FVector2D(255, 255);
-
-	uint8 LUTb[256];
-	uint8 LUTl[256];
-	uint8 LUTs[256];
-	uint8 LUTa[256];
 
 	UPROPERTY(EditDefaultsOnly, Category = "Perspective Wrap")
 		uint32 Offset = 50;
@@ -128,27 +91,18 @@ public:
 	UPROPERTY(EditDefaultsOnly, Category = "Perspective Wrap")
 		bool PerspectiveWarpBinary = true;
 
-	Point2f srcPts[4];
-	Point2f destPts[4];
+	cv::Point2f srcPts[4];
+	cv::Point2f destPts[4];
 
 
 	UPROPERTY(EditDefaultsOnly, Category = "Histogram")
 		bool ShowHistogram = true;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Histogram")
-		bool Dilate = false;
-	UPROPERTY(EditDefaultsOnly, Category = "Histogram")
-		bool Blur = false;
-	UPROPERTY(EditDefaultsOnly, Category = "Histogram")
-		bool UseBetterMethod = false;
 
 	UPROPERTY(EditAnywhere)
 		class UImageProcessor* ImageProcesingUnit = nullptr;
 
 
 public:
-	//functions
-	cv::Mat da;
 	// reads the current video frame
 	UFUNCTION(BlueprintCallable, Category = Data)
 		void ReadFrame();
@@ -158,27 +112,13 @@ public:
 		void SetThresholds(int32 index, FVector2D threshold, bool useThreshold);
 
 
-
-
 	void CreateTextures(cv::Mat& finalImage, cv::Mat& colorData);
 
 	void CreateAndDrawPerspectiveLines(cv::Mat& colorData);
-
-	void GetHLS(Mat inputRGB, Mat& outputH, Mat& outputL, Mat& outputS);
-
-	void GetLAB(Mat inputRGB, Mat& outputL, Mat& outputA, Mat& outputB);
 	
-	Mat DrawHistogram(Mat& hist);
+	cv::Mat DrawHistogram(cv::Mat& hist);
 
-	Mat BinaryThresholdLAB_LUV(Mat inputRGB, const FVector2D bThreshold, const FVector2D lThreshold);
-
-	Mat GradientThreshold(Mat inputRGB, int channel, const FVector2D threshold);
-	
-	void CreateLUT(uint8* LUT, FVector2D Threshold);
-
-	void GetHistogramPeaksFirstMethod(Mat& hist, Point2i& leftMax, Point2i& rightMax);
-
-	void GetHistogramPeaksFinalMethod(Mat& hist, Point2i& leftMax, Point2i& rightMax);
+	void GetHistogramPeaksFinalMethod(cv::Mat& hist, cv::Point2i& leftMax, cv::Point2i& rightMax);
 
 	void Save();
 	void Load();
